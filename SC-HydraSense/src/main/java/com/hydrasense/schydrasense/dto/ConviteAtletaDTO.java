d241e49147b3fd5c6c980338c7ecd853b945a41e
Gabriel Silva <donnerff076@gmail.com>
2026-05-13 17:03:09 -0300

    Feature: Cria Data Transfer Object.
    objetivo:
     - Transferir dados sem a necessidade de criar um atleta da classe Atleta.

---
+++ SC-HydraSense/src/main/java/com/hydrasense/schydrasense/dto/ConviteAtletaDTO.java
@@ -0,0 +1,13 @@
+package com.hydrasense.schydrasense.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ConviteAtletaDTO {
+
+    private String nome;
+    private String email;
+    private String codigoEquipe;
+}
