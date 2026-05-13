8ce4eed16cc79d11ca2a3e8daf21acc23ea081d8
Gabriel Silva <donnerff076@gmail.com>
2026-05-13 15:54:22 -0300

    Feature: Implementa serviço de envio de código de acesso para atletas.

---
+++ SC-HydraSense/src/main/java/com/hydrasense/schydrasense/service/EmailService.java
@@ -0,0 +1,28 @@
+package com.hydrasense.schydrasense.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void enviarCodigo(String destino, String codigo) {
+
+        SimpleMailMessage mensagem = new SimpleMailMessage();
+
+        mensagem.setTo(destino);
+        mensagem.setSubject("Código de acesso HydraSense");
+
+        mensagem.setText(
+                "Seu código de acesso é: " + codigo +
+                        "\n\nUse este código para ativar sua conta no sistema."
+        );
+
+        mailSender.send(mensagem);
+    }
+}
