96e8da50d47e2e205c415692efd3c004dc0ff0ff
Gabriel Silva <gabrielconceicaosilva.2005@gmail.com>
2026-05-18 14:21:35 -0300

    Feature: Cria AtletaRequestDTO para receber os dados do atleta cadastrado.

---
+++ SC-HydraSense/src/main/java/com/hydrasense/schydrasense/dto/AtletaRequestDTO.java
@@ -0,0 +1,17 @@
+package com.hydrasense.schydrasense.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AtletaRequestDTO {
+
+    private String nome;
+
+    private String email;
+
+    private String senha;
+
+    private String codigoEquipe;
+}
