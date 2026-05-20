64b8d9b4fd0f53b9c66c2985b673ddf9999ea1d4
Gabriel Silva <donnerff076@gmail.com>
2026-05-20 15:51:46 -0300

    Fix: Altera trecho para iniciar a entrada de dados vindo diretamente do banco de dados.

--- SC-HydraSense/Front-End/sc-hydrasense-web/src/pages/PaginasSite/Atletas.tsx
+++ SC-HydraSense/Front-End/sc-hydrasense-web/src/pages/PaginasSite/Atletas.tsx
@@ -122,28 +122,55 @@ export function Atletas() {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>
-                                <div className="td-atleta">
-                                    <div className="avatar-placeholder" />
-                                    <div className="atleta-info">
-                                        <span className="atleta-nome">Carlos Silva</span>
-                                        <span className="atleta-id">#10</span>
+                        {atletas.map((atleta: any) => (
+                            <tr key={atleta.id}>
+                                <td>
+                                    <div className="td-atleta">
+                                        <div className="avatar-placeholder" />
+
+                                        <div className="atleta-info">
+                                            <span className="atleta-nome">
+                                                {atleta.nome}
+                                            </span>
+
+                                            <span className="atleta-id">
+                                                #{atleta.id}
+                                            </span>
+                                        </div>
                                     </div>
-                                </div>
-                            </td>
-                            <td>São Paulo FC</td>
-                            <td>1.8%</td>
-                            <td>1.5 L/h</td>
-                            <td>
-                                <div className="td-status moderado">
-                                    <span className="status-dot"></span>
-                                    <span className="status-text">MODERADO</span>
-                                </div>
-                            </td>
-                            <td>95%</td>
-                            <td>15 Jun, 2024</td>
-                        </tr>
+                                </td>
+
+                                <td>
+                                    {atleta.clube?.nome || 'Sem clube'}
+                                </td>
+
+                                <td>
+                                    {atleta.pesoAtual || 0} kg
+                                </td>
+
+                                <td>
+                                    --
+                                </td>
+
+                                <td>
+                                    <div className="td-status moderado">
+                                        <span className="status-dot"></span>
+
+                                        <span className="status-text">
+                                            ATIVO
+                                        </span>
+                                    </div>
+                                </td>
+
+                                <td>
+                                    --
+                                </td>
+
+                                <td>
+                                    --
+                                </td>
+                            </tr>
+                        ))}
                     </tbody>
                 </table>
             </section>
