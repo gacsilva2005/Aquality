d2842d98bf642bba8641dd5a237ff2713d31d3ac
Gabriel Silva <donnerff076@gmail.com>
2026-05-20 17:31:46 -0300

    Feature: Nova biblioteca implementada no site para trabalhar contextos no código.

--- SC-HydraSense/Front-End/sc-hydrasense-web/src/main.tsx
+++ SC-HydraSense/Front-End/sc-hydrasense-web/src/main.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App' 
 import './index.css'  
 import { ToastProvider } from './context/ToastContext'
-import { UserProvider } from './contexts/UserContext';
+import { UserProvider } from './context/UserContext';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
