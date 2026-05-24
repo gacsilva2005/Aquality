c9aeb4696abff92b9ebbcae6d903fc47fbf24c7b
Sergio Oliveira <sholiveira.sho@gmail.com>
2026-05-24 19:41:44 -0300

    feat: expor busca de hidratacao por atleta e liberar rotas do mobile no spring security

--- SC-HydraSense/Back-End/src/main/java/com/hydrasense/schydrasense/repository/RegistroDeHidratacaoRepository.java
+++ SC-HydraSense/Back-End/src/main/java/com/hydrasense/schydrasense/repository/RegistroDeHidratacaoRepository.java
@@ -2,6 +2,8 @@ package com.hydrasense.schydrasense.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.hydrasense.schydrasense.model.RegistroDeHidratacao;
+import java.util.List;
 
 public interface RegistroDeHidratacaoRepository extends JpaRepository<RegistroDeHidratacao, Long> {
+    List<RegistroDeHidratacao> findByAtletaId(Long atletaId);
 }
