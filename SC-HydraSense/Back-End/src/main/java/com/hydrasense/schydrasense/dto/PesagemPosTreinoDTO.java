ac3a8702f5ffadaeecbfd352a2917ce61e171e4a
Gabriel Silva <gabrielconceicaosilva.2005@gmail.com>
2026-05-28 11:51:49 -0300

    Fix: Altera DTO de pesagem pós treino para passagem de hidratação também e persistência dos dados.

--- SC-HydraSense/Back-End/src/main/java/com/hydrasense/schydrasense/dto/PesagemPosTreinoDTO.java
+++ SC-HydraSense/Back-End/src/main/java/com/hydrasense/schydrasense/dto/PesagemPosTreinoDTO.java
@@ -1,11 +1,6 @@
 package com.hydrasense.schydrasense.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-public class PesagemPosTreinoDTO {
-
-    private Float pesoPosTreino;
-}
+public record PesagemPosTreinoDTO(
+        Float pesoPosTreino,
+        Integer hidratacaoMl
+) {}
