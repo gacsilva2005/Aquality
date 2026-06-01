d03e77d83ef3b7a6468c3c8c469ddd7ba5289052
Gabriel Silva <gabrielconceicaosilva.2005@gmail.com>
2026-06-01 10:03:53 -0300

    Feature: Implementa função do localStorage no UserContext.tsx para salvar dentro da constante.

--- SC-HydraSense/Front-End/sc-hydrasense-web/src/context/UserContext.tsx
+++ SC-HydraSense/Front-End/sc-hydrasense-web/src/context/UserContext.tsx
@@ -27,7 +27,25 @@ const UserContext = createContext<UserContextType>({
 
 export function UserProvider({ children }: { children: ReactNode }) {
 
-    const [user, setUser] = useState<User | null>(null);
+    const [user, setUserState] = useState<User | null>(() => {
+        const usuarioSalvo = localStorage.getItem("usuarioLogado");
+
+        if (usuarioSalvo) {
+            return JSON.parse(usuarioSalvo);
+        }
+
+        return null;
+    });
+
+    const setUser = (user: User | null) => {
+        setUserState(user);
+
+        if (user) {
+            localStorage.setItem("usuarioLogado", JSON.stringify(user));
+        } else {
+            localStorage.removeItem("usuarioLogado");
+        }
+    };
 
     return (
         <UserContext.Provider value={{ user, setUser }}>
