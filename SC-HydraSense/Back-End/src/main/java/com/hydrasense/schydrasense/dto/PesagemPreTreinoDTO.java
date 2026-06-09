d3567964c22e7d7a5cc7d13690ddb30fd7b9d59b
Gabriel Silva <gabrielconceicaosilva.2005@gmail.com>
2026-06-09 18:01:44 -0300

    Fix: Adiciona checklist DTO para passagem das informações pré treino.

--- SC-HydraSense/Back-End/src/main/java/com/hydrasense/schydrasense/dto/PesagemPreTreinoDTO.java
+++ SC-HydraSense/Back-End/src/main/java/com/hydrasense/schydrasense/dto/PesagemPreTreinoDTO.java
@@ -1,6 +1,25 @@
 package com.hydrasense.schydrasense.dto;
 
-public record PesagemPreTreinoDTO(
-        Float pesoPreTreino,
-        String sintomas
-) {}
+import lombok.Data;
+
+@Data
+public class PesagemPreTreinoDTO {
+
+    private Float pesoPreTreino;
+    private Integer corUrina;
+    private Integer sede;
+
+    private Double descontoKitGramas;
+    private String sintomas;
+    private ChecklistDTO checklist;
+
+    @Data
+    public static class ChecklistDTO {
+        private Boolean bexiga;
+        private Boolean balancaCorreta;
+        private Boolean superficiePlana;
+        private Boolean vestimentaCorreta;
+        private Boolean semCalcados;
+        private Boolean semAcessorios;
+    }
+}
